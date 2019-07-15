c064677515912af64b654be28007f2bdb3ac8cd7
unknown <rmtestard@gmail.com>
2019-07-15 20:01:52 +0200

    lineup page

--- src/components/Lineup.js
+++ src/components/Lineup.js
@@ -1,8 +1,62 @@
 import React from 'react';
 import Baselayout from '../layouts/Baselayout';
+import { Grid } from '@material-ui/core';
 
 const Lineup = () => {
-  return <Baselayout>Lineup</Baselayout>;
+  return (
+    <Baselayout>
+      <div className="LineupWrapper">
+        <Grid container spacing={0} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+        </Grid>
+        <Grid container spacing={1} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+          <Grid item xs={0}>
+            Kamaka
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+          <Grid item xs={0}>
+            Kamaka
+          </Grid>
+          <Grid item xs={0}>
+            Roloko
+          </Grid>
+        </Grid>
+        <Grid container spacing={2} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+          <Grid item xs={0}>
+            Kamaka
+          </Grid>
+          <Grid item xs={0}>
+            Roloko
+          </Grid>
+        </Grid>
+        <Grid container spacing={1} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+          <Grid item xs={0}>
+            Kamaka
+          </Grid>
+        </Grid>
+        <Grid container spacing={0} alignItems="center" justify="center">
+          <Grid item xs={0}>
+            Banzai
+          </Grid>
+        </Grid>
+      </div>
+    </Baselayout>
+  );
 };
 
 export default Lineup;
