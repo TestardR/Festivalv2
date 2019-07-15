430f06efb50a65c54b3b5a72d87f42d7522e4fd7
unknown <rmtestard@gmail.com>
2019-07-15 20:39:45 +0200

    Hebergement setup

--- src/components/Hebergement.js
+++ src/components/Hebergement.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Baselayout from '../layouts/Baselayout';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
@@ -8,7 +7,7 @@ import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Sleep1 from '../static/images/cards/cook1.jpg';
+import Cook1 from '../static/images/cards/cook1.jpg';
 import { Grid } from '@material-ui/core';
 
 const useStyles = makeStyles({
@@ -19,7 +18,8 @@ const useStyles = makeStyles({
     height: 140
   },
   light: {
-    fontWeight: 400
+    fontWeight: 400,
+    fontSize: '1rem'
   }
 });
 
@@ -33,14 +33,23 @@ const Hebergement = () => {
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image={Sleep1}
+                image={Cook1}
                 title="sweet cooking"
               />
               <CardContent>
-                <Typography variant="h6" className={classes.light}>
-                  Goody Sleep
+                <Typography
+                  variant="h6"
+                  className={classes.light}
+                  align="center"
+                >
+                  Sweet Mouth
                 </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  align="justify"
+                >
                   Lizards are a widespread group of squamate reptiles, with over
                   6,000 species, ranging across all continents except Antarctica
                 </Typography>
@@ -54,14 +63,23 @@ const Hebergement = () => {
             <CardActionArea>
               <CardMedia
                 className={classes.media}
-                image={Sleep1}
+                image={Cook1}
                 title="sweet cooking"
               />
               <CardContent>
-                <Typography variant="h6" className={classes.light}>
-                  Tenty Sleep
+                <Typography
+                  variant="h6"
+                  className={classes.light}
+                  align="center"
+                >
+                  Hot Vanilla Shake
                 </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
+                <Typography
+                  variant="body2"
+                  color="textSecondary"
+                  component="p"
+                  align="justify"
+                >
                   Lizards are a widespread group of squamate reptiles, with over
                   6,000 species, ranging across all continents except Antarctica
                 </Typography>
