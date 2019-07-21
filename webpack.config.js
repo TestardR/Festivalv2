208c7d0a23369180b1914c784045212d40a4a255
unknown <rmtestard@gmail.com>
2019-07-21 13:26:01 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+const webpackMerge = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const modeConfiguration = env => require(`./build-utils/webpack.${env}`)(env);
+
+module.exports = ({ mode } = { mode: 'production' }) => {
+  console.log(`mode is: ${mode}`);
+
+  return webpackMerge(
+    {
+      mode,
+      entry: './src/index.js',
+      devServer: {
+        hot: true,
+        open: true
+      },
+      output: {
+        publicPath: '/',
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundled.js'
+      },
+      module: {
+        rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+          },
+          {
+            test: /\.(jpe?g|png|gif|svg)$/,
+            use: [
+              {
+                loader: 'url-loader',
+                options: { limit: 40000 }
+              },
+              'image-webpack-loader'
+            ]
+          }
+        ]
+      },
+      plugins: [
+        new HtmlWebpackPlugin({
+          template: './public/index.html'
+        }),
+        new webpack.HotModuleReplacementPlugin()
+      ]
+    },
+    modeConfiguration(mode)
+  );
+};
